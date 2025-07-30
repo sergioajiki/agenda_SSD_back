5e66266cb08db01a7dd216ec63a9576deff3806e
Sergio Ajiki <sergioajk@hotmail.com>
2025-07-30 01:50:48 -0300

    feat: Primeiro commit - Projeto AgendaSSD

---
+++ src/main/java/com/ssd/agenda_SSD_back/AgendaSsdBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.agenda_SSD_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSsdBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaSsdBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssd/agenda_SSD_back/AgendaSsdBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.agenda_SSD_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSsdBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
