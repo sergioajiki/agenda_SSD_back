2534075417c247c66a2b4b222927bc8240a48fdc
SAjiki <sergioajk@hotmail.com>
2025-07-30 15:26:53 -0400

    feat: UserDto

--- src/main/java/com/ssd/agenda_SSD_back/dto/UserDto.java
+++ src/main/java/com/ssd/agenda_SSD_back/dto/UserDto.java
@@ -1,4 +1,51 @@
 package com.ssd.agenda_SSD_back.dto;
 
+import com.ssd.agenda_SSD_back.entity.User;
+
 public class UserDto {
+    private String name;
+    private String email;
+    private String password;
+    private Long matricula;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public static User toEntity(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword()); //Será criptografada no Service
+        user.setMatricula(userDto.getMatricula());
+        return user;
+    }
 }
