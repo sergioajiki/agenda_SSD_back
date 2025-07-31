90321a3e04b0648473c9dc8bfdbf4ebe37a57a90
Sergio Ajiki <sergioajk@hotmail.com>
2025-07-31 01:19:28 -0300

    feat: Meeting entity

--- src/main/java/com/ssd/agenda_SSD_back/entity/Meeting.java
+++ src/main/java/com/ssd/agenda_SSD_back/entity/Meeting.java
@@ -10,14 +10,64 @@ public class Meeting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 150)
     private String title;
-    @Column(nullable = false)
-    private LocalDate data;
-    @Column(nullable = false)
-    private LocalTime timeStart;
-    @Column(nullable = false)
-    private LocalTime timeEnd;
-    @Column(nullable = false)
-    private User userId;
+    @Column(name="meet_date", nullable = false)
+    private LocalDate meetDate; // D+ata no formato yyyy-MM-dd
+    @Column(name="time_start", nullable = false)
+    private LocalTime timeStart; // Horário (hh:mm)
+    @Column(name="time_end", nullable = false)
+    private LocalTime timeEnd; // Horário (hh:mm)
+
+    @ManyToOne
+    @JoinColumn(name="host_user", nullable = false)
+    private User hostUser;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getMeetDate() {
+        return meetDate;
+    }
+
+    public void setMeetDate(LocalDate meetDate) {
+        this.meetDate = meetDate;
+    }
+
+    public LocalTime getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(LocalTime timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public LocalTime getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(LocalTime timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public User getHostUser() {
+        return hostUser;
+    }
+
+    public void setHostUser(User hostUser) {
+        this.hostUser = hostUser;
+    }
 }
