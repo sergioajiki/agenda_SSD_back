900dff9a830cc45a5931e5263e25026f663dc5ca
SAjiki <sergioajk@hotmail.com>
2025-08-04 15:09:43 -0400

    feat: Business Rule Exception para regra de negócios

---
+++ src/main/java/com/ssd/agenda_SSD_back/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package com.ssd.agenda_SSD_back.exception;
+
+public class BusinessRuleException extends RuntimeException{
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
