f9b3a036f9211451234d6e8264a9b125d1170c5f
SAjiki <sergioajk@hotmail.com>
2025-08-04 15:14:28 -0400

    feat: NotFoundException

---
+++ src/main/java/com/ssd/agenda_SSD_back/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ssd.agenda_SSD_back.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
