9fddb507bd8b87b8b4e49736c379442e42192f34
SAjiki <sergioajk@hotmail.com>
2025-08-08 10:05:03 -0400

    feat: incluida validação de formato de email no em UserService

--- src/main/java/com/ssd/agenda_SSD_back/repository/UserRepository.java
+++ src/main/java/com/ssd/agenda_SSD_back/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.ssd.agenda_SSD_back.entity.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/ssd/agenda_SSD_back/service/UserService.java
+++ src/main/java/com/ssd/agenda_SSD_back/service/UserService.java
@@ -1,7 +1,11 @@
 package com.ssd.agenda_SSD_back.service;
 
+import com.ssd.agenda_SSD_back.exception.DuplicateEntryException;
+import com.ssd.agenda_SSD_back.exception.InvalidEmailFormatException;
+import com.ssd.agenda_SSD_back.exception.NotFoundException;
 import com.ssd.agenda_SSD_back.repository.UserRepository;
 import com.ssd.agenda_SSD_back.entity.User;
+import com.ssd.agenda_SSD_back.util.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +16,14 @@ public class UserService {
 
     public User createUser(User newUser) {
         //Criar uma verificação de formato de email válido
-
+        boolean isEmail = EmailValidator.isValidEmail(newUser.getEmail());
+        if (!isEmail) {
+            throw new InvalidEmailFormatException("Invalid email format");
+        }
         //Criar uma verificação se o email já está cadastrado
+        if (userRepository.findByEmail(newUser.getEmail()).isPresent()) {
+            throw new DuplicateEntryException("Email já cadastrado");
+        }
 
         //Criptografar a senha antes de salvar no BD
 
@@ -22,9 +32,7 @@ public class UserService {
 
     public User findUserById(Long id) {
 
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException(
-                "Usuário não encontrado com ID "+ id ));
-
-        //Criar NotFoundExcepiton para substituir RuntimeException
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException(
+                "Usuário não encontrado com ID " + id));
     }
 }
